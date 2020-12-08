a6c6726869d4a65d14a98d366749e57f7ca5c66c
ma22npk <gunjo64@icloud.com>
2020-12-09 00:50:04 +0900

    プログラムをラムダ式に書き換える

--- src/about_lambda_expression1/Main.java
+++ src/about_lambda_expression1/Main.java
@@ -4,34 +4,12 @@ public class Main {
 
     // Eclipse上で「実行の構成」引数タブ「コマンド引数」に任意の値で出力される内容が変わる。
     public static void main(String[] args) {
+        // ラムダ式に書き換える
+        Runnable morningRunnable = () -> System.out.println("おはよう！");
+        Runnable AfternoonRunnable = () -> System.out.println("こんにちは！");
+        Runnable EveningRunnable = () -> System.out.println("こんばんわ！");
+        Runnable OtherRunnable = () -> System.out.println("もう早く寝たほうがいいですよ");
 
-        Runnable morningRunnable = new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("おはよう！");
-            }
-        };
-
-        Runnable AfternoonRunnable = new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("こんにちは！");
-            }
-        };
-
-        Runnable EveningRunnable = new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("こんばんわ！");
-            }
-        };
-
-        Runnable OtherRunnable = new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("もう早く寝たほうがいいですよ");
-            }
-        };
         int myTime = Integer.parseInt(args[0]);
         if (5 <= myTime && myTime <= 10) {
             morningRunnable.run();
